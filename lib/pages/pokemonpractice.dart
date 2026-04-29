05d8571c1b98fdec3bc7dce2217b8d73d738e201
Hang Lifong <hanglyfong777@gmail.com>
2026-04-29 16:29:26 +0700

    update pokemon refresh

--- lib/pages/pokemonpractice.dart
+++ lib/pages/pokemonpractice.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:math';
 
 class Pokemonpractice extends StatefulWidget {
   const Pokemonpractice({super.key});
@@ -18,8 +19,13 @@ class _PokemonpracticeState extends State<Pokemonpractice> {
     super.initState();
     fetchPokemon();
   }
-    Future<void> fetchPokemon() async {
-    final url = Uri.parse('https://pokeapi.co/api/v2/pokemon/pikachu');
+
+  Future<void> fetchPokemon() async {
+    //random number
+    var number = Random();
+    int randomId = number.nextInt(1000) + 1;
+
+    final url = Uri.parse('https://pokeapi.co/api/v2/pokemon/$randomId');
 
     final response = await http.get(url);
 
@@ -37,28 +43,33 @@ class _PokemonpracticeState extends State<Pokemonpractice> {
       });
     }
   }
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-  appBar: AppBar(
-    title: const Text('Pokemon Viewer'),
-  ),
-  body: Center(
-    child: isLoading
-        ? const CircularProgressIndicator()
-        : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                name.toUpperCase(),
-                style: const TextStyle(fontSize: 24),
+      appBar: AppBar(title: const Text('Pokemon Viewer')),
+      body: Center(
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  imageUrl.isNotEmpty
+                      ? Image.network(imageUrl)
+                      : const SizedBox(height: 100,),
+                  Text(
+                    name.toUpperCase(),
+                    style: const TextStyle(fontSize: 24),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {
+                      fetchPokemon();
+                    },
+                    child: const Text("Refresh"),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              Image.network(imageUrl),
-            ],
-          ),
-  ),
-);
+      ),
+    );
   }
 }
-
-
